000f64d92e0038e4dd5d74ad6dee89922e079467
Toastx <toastx.sol@gmail.com>
2025-09-28 03:25:08 +0530

    feat: :sparkles: added token launch and staking contracts

---
+++ token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
